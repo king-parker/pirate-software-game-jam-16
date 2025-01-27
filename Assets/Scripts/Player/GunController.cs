7012a1b51ced79d2ebe47d4af8e1cde2860f2867
Parker King <gnikrekrap@gmail.com>
2025-01-26 19:54:09 -0700

    Bullet time base implementation

--- Assets/Scripts/Player/GunController.cs
+++ Assets/Scripts/Player/GunController.cs
@@ -13,14 +13,35 @@ public class GunController : MonoBehaviour
     [SerializeField] private ParticleSystem shotFlashParticles;
     [SerializeField] private Transform flashPoint;
 
+    [Header("Bulle Time")]
+    [SerializeField][Range(0f, 1f)] private float bulletTimeScale = 0.5f;
+
     private KeyCode m_shootKey = KeyCode.Space;
+    private KeyCode m_bulletTimeKey = KeyCode.LeftShift;
     private bool m_applyRecoil = false;
+    private float m_regularFixedDeltaTime;
+    private float m_regularTimeScale = 1f;
+    private bool m_bulletTimeActive;
+
+    private void Start()
+    {
+        m_regularFixedDeltaTime = Time.fixedDeltaTime;
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(m_shootKey))
         {
             FireGun();
+        }
+
+        if (Input.GetKeyDown(m_bulletTimeKey))
+        {
+            StartBulletTime();
+        }
+        if (Input.GetKeyUp(m_bulletTimeKey))
+        {
+            StopBulletTime();
         }
     }
 
@@ -51,15 +72,28 @@ public class GunController : MonoBehaviour
         var direction = Mathf.Sign(forward.x);
         var sine = forward.y;
         var recoilTorque = direction * Mathf.Max(minTorque, Mathf.Abs(sine) * fireTorque);
-        /*
-         * NOTES: If the gun is perfectly level, then no rotation is applied. Most of the time this
-         * should not be an issue except at the start. The following are potential fixes.
-         * 1. Ensure the torque is never zero
-         * 2. Spawn the gun with a slight rotation at the start
-         */ 
+
+        // Scale forces up durring bullet time to maintain consistent physics
+        float bulletTimeFactor = m_bulletTimeActive ? 1f / bulletTimeScale : 1f;
+        recoilForce *= bulletTimeFactor;
+        recoilTorque *= bulletTimeFactor;
 
         rb.AddForce(recoilForce);
         rb.angularVelocity = 0;
         rb.AddTorque(recoilTorque);
+    }
+
+    private void StartBulletTime()
+    {
+        m_bulletTimeActive = true;
+        Time.timeScale = bulletTimeScale;
+        Time.fixedDeltaTime = m_regularFixedDeltaTime * bulletTimeScale;
+    }
+
+    private void StopBulletTime()
+    {
+        m_bulletTimeActive = false;
+        Time.timeScale = m_regularTimeScale;
+        Time.fixedDeltaTime = m_regularFixedDeltaTime;
     }
 }
